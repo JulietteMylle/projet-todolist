c7fa011bedc2b2e6e21dc7659e5c0c7af8ea1976
Juliette Mylle <154231973+JulietteMylle@users.noreply.github.com>
2024-01-18 18:00:37 +0100

    Add files via upload

---
+++ Projet_To_Do/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once 'includes/head.php'?>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+
+
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma To-do list !</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php'?>
+
+
+    </div>
+
+</body>
+
+</html>
